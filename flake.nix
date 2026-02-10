bc7d10248d0dffe3e5b7802f80cfa4ed35a0f7f0
Samuka007 <samuka007@dragonos.org>
2026-02-10 12:13:48 +0800

    feat: 在 flake.nix 中添加 aarch64-darwin 系统支持；修正 logmonitor 的 klog_types 路径

--- flake.nix
+++ flake.nix
@@ -13,6 +13,7 @@
   outputs = inputs@{ self, nixpkgs, fenix, flake-parts }: flake-parts.lib.mkFlake { inherit inputs; } {
     systems = [
       "x86_64-linux"
+      "aarch64-darwin"
     ];
     perSystem = { self', inputs', system, ... }:
       let
